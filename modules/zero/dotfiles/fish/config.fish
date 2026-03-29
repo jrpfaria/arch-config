593ea67e7771c0343a765334ddbdb3ea9b2880d4
Rodrigo <jrfaria0504@gmail.com>
2026-03-29 16:57:06 +0100

    keeping things up to date

--- modules/zero/dotfiles/fish/config.fish
+++ modules/zero/dotfiles/fish/config.fish
@@ -11,15 +11,25 @@ export QT_QPA_PLATFORMTHEME="qt6ct"
 #
 
 function fix-window
-    if test (count $argv) -eq 0
-        echo "Usage: fix-window <pid> (run 'hyprctl clients' to check intended pid)"
-        return 1
-    end
+  if test (count $argv) -eq 0
+    echo "Usage: fix-window <pid> (run 'hyprctl clients' to check intended pid)"
+    return 1
+  end
 
-    for pid in $argv
-        hyprctl dispatch focuswindow "pid:$pid"
-        hyprctl dispatch centerwindow
-    end
+  for pid in $argv
+    hyprctl dispatch focuswindow "pid:$pid"
+    hyprctl dispatch centerwindow
+  end
+end
+
+function pdf
+  if test (count $argv) -eq 0
+    echo "Usage: pdf <file>"
+    return 1
+  end
+
+  zen-browser $argv & disown
+  exit
 end
 
 alias off='shutdown -h now'
@@ -28,7 +38,7 @@ alias vv='nvim .'
 alias install='dcli install'
 alias search='paru -Ss --bottomup'
 alias uninstall='dcli remove'
-alias pdf='zen-browser'
+alias get_idf='. $HOME/esp/esp-idf/export.fish'
 
 zoxide init fish | source
 starship init fish | source
